72bc9d56e4c4c65802a9efdf79ae0dad413faf7d
smallest-iota <148168035+smallest-iota@users.noreply.github.com>
2024-04-03 20:38:03 -0500

    Updated setup.sh

--- dotfiles/scripts/setup.sh
+++ dotfiles/scripts/setup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# Moves all files within folder to the main location.
+# Moves all files within folder to ~/.config.
 mkdir -p ~/.config/dotfiles/
-cp -a ./. ~/.config/dotfiles/
+if [ cp -a ./. ~/.config/dotfiles/ ]; then
+    echo "[@] Copied files to .config"
+fi
 
-# i3wm config, i3wm expects the configuration files at
-# ~/.config/dotfiles/configs.
-cp -a ./configs/config ~/.config/i3/
+CONFIGS_DIR=~/.config/dotfiles/configs
+
+# Neovim config, neovim expects the configuration files at:
+# ~/.config/nvim/
+mkdir -p ~/.config/nvim/
+if [ cp -a $CONFIGS_DIR/nvim/. ~/.config/nvim/ ]; then
+    echo "[@] Copied files to .config/nvim/"
+fi
+
+# i3wm config, i3wm expects the configuration files at:
+# ~/.config/i3/
+if [ cp -a $CONFIGS_DIR/config ~/.config/i3/ ]; then
+    echo "[@] Copied files to .config/i3/"
+fi
