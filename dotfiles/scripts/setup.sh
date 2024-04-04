e9743309733d6826173371b1740eb9692534c913
smallest-iota <148168035+smallest-iota@users.noreply.github.com>
2024-04-04 09:47:30 -0500

    Refactored structure
    - Refactored file structure
    - Updated config paths

--- dotfiles/scripts/setup.sh
+++ dotfiles/scripts/setup.sh
@@ -8,15 +8,16 @@ fi
 
 CONFIGS_DIR=~/.config/dotfiles/configs
 
-# Neovim config, neovim expects the configuration files at:
-# ~/.config/nvim/
+# Neovim config, neovim expects the configuration files at: ~/.config/nvim/
 mkdir -p ~/.config/nvim/
 if [ cp -a $CONFIGS_DIR/nvim/. ~/.config/nvim/ ]; then
     echo "[@] Copied files to .config/nvim/"
 fi
 
-# i3wm config, i3wm expects the configuration files at:
-# ~/.config/i3/
-if [ cp -a $CONFIGS_DIR/config ~/.config/i3/ ]; then
+# Note; i3 config references, $CONFIGS_DIR/alacritty/... && $CONFIGS_DIR/picom/...
+# So edit the files at those directories in order to change the alacritty and picom configurations.
+
+# i3wm config, i3wm expects the configuration files at: ~/.config/i3/
+if [ cp -a $CONFIGS_DIR/i3/config ~/.config/i3/ ]; then
     echo "[@] Copied files to .config/i3/"
 fi
