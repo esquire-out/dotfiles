75579f6222fdc562489e71cd47edaf389f78622e
smallest-iota <148168035+smallest-iota@users.noreply.github.com>
2024-04-15 06:09:09 -0500

    Updated Icons

--- dotfiles/configs/nvim/lua/comments.lua
+++ dotfiles/configs/nvim/lua/comments.lua
@@ -1,13 +1,13 @@
 
--- TODO:
--- FIX:
--- NOTE:
--- HACK:
--- BUG:
--- WARN:
--- INFO:
--- PERF:
--- TEST:
+-- TODO: foo bar
+-- FIX: ris
+-- NOTE: lgjkr
+-- HACK: djwhqh
+-- BUG: dasd
+-- WARN: dqweqwe
+-- INFO: l;jl
+-- PERF: iojko
+-- TEST: 123924jiop3j
 
 require('todo-comments').setup({
   signs = true, -- show icons in the signs column
@@ -15,17 +15,17 @@ require('todo-comments').setup({
   -- keywords recognized as todo comments
   keywords = {
     FIX = {
-      icon = " ", -- icon used for the sign, and in search results
+      icon = "", -- icon used for the sign, and in search results
       color = "error", -- can be a hex color, or a named color (see below)
       alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
       -- signs = false, -- configure signs for some keywords individually
     },
-    TODO = { icon = " ", color = "info" },
-    HACK = { icon = " ", color = "warning" },
-    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+    TODO = { icon = "", color = "info" },
+    HACK = { icon = "", color = "default" },
+    WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
     NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    TEST = { icon = "", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
   },
   colors = {
     error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
